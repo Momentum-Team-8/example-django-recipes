9fde3efd5574839c3745b55f9bf7292e9e0944de
Amy Gori <amydgori@gmail.com>
2021-07-15 17:01:58 -0400

    Add endpoint for creating new recipes

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ This project was generated from the Momentum Django project template. This templ
 - There is a `templates/` and a `static/` directory at the top level, both of which are set up to be used.
 - A `.gitignore` file is provided.
 - [Pipenv](https://pipenv.pypa.io/en/latest/) is used to manage dependencies.
+
+
+## Django REST Framework
+
+### How to make a request to `POST api/recipes` in Insomnia
+
+![Screencast of making a POST request to the API](POST.gif)
--- api/views.py
+++ api/views.py
@@ -14,3 +14,23 @@ class RecipeListView(APIView):
         recipes = Recipe.objects.all()
         serializer = RecipeSerializer(recipes, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        # we're creating a new recipe with data from the POST request
+        # we get that data using request.data
+        # check it out in the DRF docs: https://www.django-rest-framework.org/api-guide/requests/#data
+        serializer = RecipeSerializer(data=request.data)
+
+        # Now we have to validate this incoming data before we can save it.
+        # "When deserializing data, you always need to call is_valid() before attempting to access the validated data, or save an object instance."
+        # https://www.django-rest-framework.org/api-guide/serializers/#validation
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            # ☝️ This is like how we can call form.save() to save the new instance
+            # when we wanted to save an associated user, we had to do it in two steps using `commit=False`
+            # DRF lets us do it in one clear step without `commit=False`
+            return Response(serializer.data)
+
+        # "If any validation errors occur, the .errors property will contain a dictionary representing the resulting error messages."
+        # we can use the errors information in our response in case the data is not valid
+        return Response(serializer.errors)
